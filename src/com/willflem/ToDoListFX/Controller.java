8d8b6de228155cf4d41983d223ed8313335fccf7
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-05-07 13:06:27 -0400

    added different tasks

--- src/com/willflem/ToDoListFX/Controller.java
+++ src/com/willflem/ToDoListFX/Controller.java
@@ -12,6 +12,14 @@ public class Controller {
 
     public void initialize() {
         ToDoItem item1 = new ToDoItem("Mail birdthday card", "Buy a birthday card for Mom",
-                LocalDate.of(2019, Month.MAY, 25));
+                LocalDate.of(2019, Month.MAY, 7));
+        ToDoItem item2 = new ToDoItem("Study Java Databases", " Finish JavaFX with SQL databases",
+                LocalDate.of(2019, Month.MAY, 8));
+        ToDoItem item3 = new ToDoItem("Start React Course", "Start on course Kevin gave you",
+                LocalDate.of(2019, Month.MAY, 10));
+        ToDoItem item4 = new ToDoItem("Email Felicia", "Email Felicia information",
+                LocalDate.of(2019, Month.MAY, 7));
+        ToDoItem item5 = new ToDoItem("Clean up", "Clean apartment",
+                LocalDate.of(2019, Month.MAY, 13));
     }
 }
