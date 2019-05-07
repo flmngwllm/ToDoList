8d622012ee5f43a622be39edaf3ffdcce88caaa0
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-05-07 13:01:21 -0400

    initializes items when application starts

--- src/com/willflem/ToDoListFX/Controller.java
+++ src/com/willflem/ToDoListFX/Controller.java
@@ -1,4 +1,17 @@
 package com.willflem.ToDoListFX;
 
+import com.willflem.ToDoListFX.datamodel.ToDoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 public class Controller {
+
+    private List<ToDoItem> toDoItems;
+
+    public void initialize() {
+        ToDoItem item1 = new ToDoItem("Mail birdthday card", "Buy a birthday card for Mom",
+                LocalDate.of(2019, Month.MAY, 25));
+    }
 }
