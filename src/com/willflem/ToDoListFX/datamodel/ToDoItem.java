c4b074993f7239df6f266144ebeb2aee110fd4dd
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-05-07 12:56:41 -0400

    added getters and setters for constructors

--- src/com/willflem/ToDoListFX/datamodel/ToDoItem.java
+++ src/com/willflem/ToDoListFX/datamodel/ToDoItem.java
@@ -13,4 +13,28 @@ public class ToDoItem {
         this.details = details;
         this.deadline = deadline;
     }
+
+    public String getShortDescription(){
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription){
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails(){
+        return details;
+    }
+
+    public void setDetails(String details){
+        this.details = details;
+    }
+
+    public LocalDate getDeadline(){
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline){
+        this.deadline = deadline;
+    }
 }
